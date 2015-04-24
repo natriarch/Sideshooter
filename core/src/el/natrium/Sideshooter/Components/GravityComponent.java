9dd20e2f15d15e3f84923ca7c64dc86a1c8f4245
Nate Le <natele@utexas.edu>
2015-04-23 20:52:35 -0400

    Gravity Component a

---
+++ core/src/el/natrium/Sideshooter/Components/GravityComponent.java
@@ -0,0 +1,8 @@
+package el.natrium.Sideshooter.Components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Vector2;
+
+public class GravityComponent extends Component{
+	public final Vector2 gravity = new Vector2();
+}
