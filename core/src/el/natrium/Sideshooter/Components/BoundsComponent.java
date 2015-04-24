b9e72507e4f0775adde688b15327440b01fa75ff
Nate Le <natele@utexas.edu>
2015-04-23 20:50:39 -0400

    Gravity Component

--- core/src/el/natrium/Sideshooter/Components/BoundsComponent.java
+++ core/src/el/natrium/Sideshooter/Components/BoundsComponent.java
@@ -5,4 +5,7 @@ import com.badlogic.gdx.math.Rectangle;
 
 public class BoundsComponent extends Component{
 	public final Rectangle bounds = new Rectangle();
+	public final boolean injuring = false;
+	public final boolean injurable = false;
+	public final boolean clipping = false;
 }
